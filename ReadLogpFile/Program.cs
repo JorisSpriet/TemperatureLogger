232d7cdba8d7d6c11938110c0ac9fe72e3ec7c14
Joris Spriet <joris.spriet@brighteye.be>
2018-10-08 11:26:54 +0200

    Reading logp file for importing existing logs into db

---
+++ ReadLogpFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ReadLogpFile
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		
+	}
+}
---
+++ ReadLogpFile/TemperatureLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReadLogpFile
+{
+	public class TemperatureLog
+	{
+		public string LoggerName { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public DateTime Time { get; set; }
+
+		public float Temperature { get; set; }
+	}
+}
